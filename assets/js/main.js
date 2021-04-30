70813b16beb64589dce9505d935fab3782124731
codergo7 <codergo7@gmail.com>
2021-05-01 01:23:04 +0200

    completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,10 @@
     typed_strings = typed_strings.split(',')
     new Typed('.typed', {
       strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
+      loop: false,
+      typeSpeed: 20,
+      backSpeed: false,
+      backDelay: false
     });
   }
 
